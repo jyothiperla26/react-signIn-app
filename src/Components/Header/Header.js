998e82c5d403baba25fea24e879cd4b0f4d2b4b8
jyothiperla26 <jyothi.perla@everest.engineering>
2023-06-16 11:07:29 +0530

    The header page logic was written

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export function Header() {
+  return (
+    <>
+      <h1 className="heading">Sign-in User</h1>
+      <p className="para-element">Please fill the below details</p>
+    </>
+  );
+}
