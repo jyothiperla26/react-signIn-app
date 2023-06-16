1086880fbc45aaba1f9bcc0ae4962213cb94d5a2
jyothiperla26 <jyothi.perla@everest.engineering>
2023-06-16 11:17:52 +0530

    The two buttons logic was written

---
+++ src/Components/Bottom/Bottom.js
@@ -0,0 +1,10 @@
+import './Bottom.css';
+
+export function Bottom() {
+  return (
+    <div className = "alignButtons">
+      <button className="cancel-button">Cancel</button>
+      <button className="create-user-button">Create User</button>
+    </div>
+  );
+}
