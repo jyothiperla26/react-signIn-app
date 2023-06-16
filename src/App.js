ac1c73c9b37dcde24ee5642d446f540cfbae7754
jyothiperla26 <jyothi.perla@everest.engineering>
2023-06-16 11:02:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './Components/Header/Header';
+import { InputContainer } from './Components/Body/InputContainer';
+import { Bottom } from './Components/Bottom/Bottom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bgContainer">
+      <div>
+        <Header></Header>
+        <InputContainer></InputContainer>
+        <Bottom></Bottom>
+      </div>
     </div>
   );
 }
