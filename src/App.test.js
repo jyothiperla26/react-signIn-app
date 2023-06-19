b1e1dbc8766848743c38c79e71f6906359585510
jyothiperla26 <jyothi.perla@everest.engineering>
2023-06-19 17:26:03 +0530

    Tested some fields on the screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { Header } from './Components/Header/Header';
+
 
 test("Sign-in User", () => {
   render(<App/>);
   const name = screen.getByText("Sign-in User");
   expect(name).toBeInTheDocument();
+});
+
+test("Please fill the below details", () => {
+  render(<App/>);
+  const name = screen.getByText("Please fill the below details");
+  expect(name).toBeInTheDocument();
+});
+
+test("renders form firstname", () => {
+  render(<App/>);
+  const name = screen.getByText("First Name");
+  expect(name).toBeInTheDocument();
+});
+
+test("renders form lastname", () => {
+  render(<App/>);
+  const name = screen.getByText("Last Name");
+  expect(name).toBeInTheDocument();
+});
+
+test("renders form Role preference", () => {
+  render(<App/>);
+  const name = screen.getByText("Email");
+  expect(name).toBeInTheDocument();
+});
+
+test("renders form Role preference", () => {
+    render(<App/>);
+    const name = screen.getByText("Phone Number");
+    expect(name).toBeInTheDocument();
 });
